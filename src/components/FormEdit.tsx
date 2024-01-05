957a7afac957f2c8e0a4668c22be1965c495406a
devfritz <devfritzcode@gmail.com>
2024-01-05 14:40:18 -0500

    add editor monaco

--- src/components/FormEdit.tsx
+++ src/components/FormEdit.tsx
@@ -1,9 +1,16 @@
+'use client'
 import type { Snippet } from '@prisma/client';
+import Editor from '@monaco-editor/react';
+import { useState } from 'react';
 interface PropsEditForm {
   snippet: Snippet
 }
 function FormEdit({ snippet }: PropsEditForm) {
-  return <div>Hey {snippet.code}</div>;
+  const [code, setCode] = useState("")
+  const handleChangeEditor = (value: string = "") => {
+    setCode(value)
+  }
+  return <Editor height="40vh" theme='vs-dark' defaultLanguage="javascript" defaultValue={snippet.code} options={{ minimap: { enabled: false } }} onChange={handleChangeEditor} />;
 }
 
 export default FormEdit;
